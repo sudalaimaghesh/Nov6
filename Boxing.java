41434455401c6b9e5b52848071669c4243a04420
Magheshwari <sudalaimaghesh@gmail.com>
2020-11-06 19:48:35 +0530

    This is first commit

---
+++ Boxing.java
@@ -0,0 +1,12 @@
+class Boxing
+{
+	public static void main(String args[])
+	{
+		//boxing
+		int a = 20;
+		Integer obj;	//declaring object
+		obj = a; // autoboxing
+		System.out.println("Primitive type" +a);
+		System.out.println("Wrapper type" +obj);
+	}
+}
